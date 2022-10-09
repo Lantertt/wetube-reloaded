a75044110b7bad6400148091656565fd30c550f5
Lantertt <101917358+Lantertt@users.noreply.github.com>
2022-10-09 15:14:07 +0900

    isHeroku

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,6 +9,8 @@ const s3 = new aws.S3({
   },
 });
 
+const isHeroku = process.env.NODE_ENV === "production";
+
 const s3ImageUploader = multerS3({
   s3: s3,
   bucket: "yongtubeee/images",
@@ -25,6 +27,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -51,7 +54,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: s3ImageUploader,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
@@ -59,5 +62,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
-  storage: s3VideoUploader,
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
